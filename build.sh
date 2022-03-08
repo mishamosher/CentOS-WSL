21da520e9d1e3c21b6a9f6c36c3b9e30ace0cbd0
Elmer Miroslav Mosher Golovin <miroslav@mishamosher.com>
2022-03-08 11:37:25 +0300

    Use https for QCOW2 image download.

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ ARCH="x86_64"
 OS_VER="7"
 ROOTFS_VER="2111"
 ROOTFS_FN="CentOS-${OS_VER}-${ARCH}-GenericCloud-${ROOTFS_VER}.qcow2"
-ROOTFS_URL="http://cloud.centos.org/centos/${OS_VER}/images/${ROOTFS_FN}"
+ROOTFS_URL="https://cloud.centos.org/centos/${OS_VER}/images/${ROOTFS_FN}"
 
 # Environment variables for Yuk7's wsldl
 LNCR_BLD="22020900"
