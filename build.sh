817192702714c572f8eb184d5196a4c5fa8bda3d
Elmer Miroslav Mosher Golovin <miroslav@mishamosher.com>
2023-07-01 16:30:48 +0300

    Update CentOS 7 to 2211.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Environment variables for the CentOS cloud image
 ARCH="x86_64"
 OS_VER="7"
-ROOTFS_VER="2111"
+ROOTFS_VER="2211"
 ROOTFS_FN="CentOS-${OS_VER}-${ARCH}-GenericCloud-${ROOTFS_VER}.qcow2"
 ROOTFS_URL="https://cloud.centos.org/centos/${OS_VER}/images/${ROOTFS_FN}"
 
